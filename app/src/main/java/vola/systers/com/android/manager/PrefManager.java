5be2cc782442dcaf25f1e33a390c766712b4d900
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-03-07 01:36:05 +0530

    Update Prefmanager.java
    Added getters and setters in prefManager for different data types
    
    
    Remove unnecessary object creation

--- app/src/main/java/vola/systers/com/android/manager/PrefManager.java
+++ app/src/main/java/vola/systers/com/android/manager/PrefManager.java
@@ -3,31 +3,84 @@ package vola.systers.com.android.manager;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Set;
+
 public class PrefManager {
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-    Context _context;
+    private SharedPreferences preferences;
 
     // shared pref mode
-    int PRIVATE_MODE = 0;
+    private int PRIVATE_MODE = 0;
 
     // Shared preferences file name
-    private static final String PREF_NAME = "LoginPrefs";
+    private static final String PREF_NAME = "Volunteers_Pref";
     private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
 
     public PrefManager(Context context) {
-        this._context = context;
-        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-        editor = pref.edit();
+        preferences = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+    }
+
+    public SharedPreferences getPreferences() {
+        return preferences;
+    }
+
+    public void clear() {
+        getPreferences().edit().clear().apply();
+    }
+
+    public int getInt(String preferenceKey, int preferenceDefaultValue) {
+        return getPreferences().getInt(preferenceKey, preferenceDefaultValue);
+    }
+
+    public void putInt(String preferenceKey, int preferenceValue) {
+        getPreferences().edit().putInt(preferenceKey, preferenceValue).apply();
+    }
+
+    public long getLong(String preferenceKey, long preferenceDefaultValue) {
+        return getPreferences().getLong(preferenceKey, preferenceDefaultValue);
+    }
+
+    public void putLong(String preferenceKey, long preferenceValue) {
+        getPreferences().edit().putLong(preferenceKey, preferenceValue).apply();
+    }
+
+    public float getFloat(String preferenceKey, float preferenceDefaultValue) {
+        return getPreferences().getFloat(preferenceKey, preferenceDefaultValue);
+    }
+
+    public void putFloat(String preferenceKey, float preferenceValue) {
+        getPreferences().edit().putFloat(preferenceKey, preferenceValue).apply();
+    }
+
+    public boolean getBoolean(String preferenceKey, boolean preferenceDefaultValue) {
+        return getPreferences().getBoolean(preferenceKey, preferenceDefaultValue);
+    }
+
+    public void putBoolean(String preferenceKey, boolean preferenceValue) {
+        getPreferences().edit().putBoolean(preferenceKey, preferenceValue).apply();
+    }
+
+    public String getString(String preferenceKey, String preferenceDefaultValue) {
+        return getPreferences().getString(preferenceKey, preferenceDefaultValue);
+    }
+
+    public void putString(String preferenceKey, String preferenceValue) {
+        getPreferences().edit().putString(preferenceKey, preferenceValue).apply();
+    }
+
+    public void putStringSet(String preferenceKey, Set<String> stringSet) {
+        getPreferences().edit().putStringSet(preferenceKey, stringSet).apply();
+    }
+
+    public Set<String> getStringSet(String preferenceKey) {
+        return getPreferences().getStringSet(preferenceKey, null);
     }
 
     public void setFirstTimeLaunch(boolean isFirstTime) {
-        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
-        editor.commit();
+
+        getPreferences().edit().putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime).apply();
     }
 
     public boolean isFirstTimeLaunch() {
-        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+        return getPreferences().getBoolean(IS_FIRST_TIME_LAUNCH, true);
     }
-
 }
